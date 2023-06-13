77595e3ce6cb04eef771501de3758ea07d48f076
CelioAmaral <celioamaralibf@gmail.com>
2023-06-13 13:48:58 -0300

    feat: schedule action

--- crud-mongodb/routes/index.js
+++ crud-mongodb/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const db = require("../db");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post("/save", async (req, res) => {
+  const customer = req.body;
+  const result = await db.insert(customer);
+  console.log(result);
+  res.json(result);
+})
 
 module.exports = router;
