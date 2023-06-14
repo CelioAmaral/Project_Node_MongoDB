5e5dbcc749d97c3eac22ad039ea3a935f857e481
CelioAmaral <celioamaralibf@gmail.com>
2023-06-13 22:14:36 -0300

    feat: implementation of find function

--- crud-mongodb/db.js
+++ crud-mongodb/db.js
@@ -18,6 +18,12 @@ async function insert(customer) {
     return db.collection("customers").insertOne(customer);
 }
 
+async function find() {
+    const db = await connect();
+    return db.collection("customers").find().toArray();
+}
+
 module.exports = {
-    insert
+    insert,
+    find,
 }
