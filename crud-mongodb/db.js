fba8ff713967680fee508a0215fa1ea5b4fcfa01
CelioAmaral <celioamaralibf@gmail.com>
2023-06-14 16:19:26 -0300

    feat: full crud

--- crud-mongodb/db.js
+++ crud-mongodb/db.js
@@ -2,9 +2,10 @@ require("dotenv").config();
 const { MongoClient } = require("mongodb");
 
 let singleton;
+const COLLECTION = "customers";
 
 async function connect() {
-    if(singleton) return singleton;
+    if (singleton) return singleton;
 
     const client = new MongoClient(process.env.MONGO_HOST);
     await client.connect();
@@ -13,17 +14,42 @@ async function connect() {
     return singleton;
 }
 
+const TAMANHO_PAGINA = 5;
+
+async function findAll(pagina) {
+    const tamanhoSkip = TAMANHO_PAGINA * (pagina - 1);
+    const db = await connect();
+
+    return db.collection(COLLECTION)
+        .find()
+        .skip(tamanhoSkip)
+        .limit(TAMANHO_PAGINA)
+        .toArray();
+}
+
 async function insert(customer) {
     const db = await connect();
-    return db.collection("customers").insertOne(customer);
+    return db.collection(COLLECTION).insertOne(customer);
 }
 
-async function find() {
+async function findOne(id) {
     const db = await connect();
-    return db.collection("customers").find().toArray();
+    return db.collection(COLLECTION).findOne({_id: new ObjectId(id)});
 }
 
-module.exports = {
-    insert,
-    find,
+async function update(id, customer) {
+    const db = await connect();
+    return db.collection(COLLECTION).updateOne({ _id: new ObjectId(id) }, { $set: customer });
 }
+
+async function deleteOne(id) {
+    const db = await connect();
+    return db.collection(COLLECTION).deleteOne({ _id: new ObjectId(id) });
+}
+
+async function countAll() {
+    const db = await connect();
+    return db.collection(COLLECTION).countDocuments();
+}
+
+module.exports = { findAll, insert, findOne, update, deleteOne, countAll, TAMANHO_PAGINA }
--- crud-mongodb/routes/index.js
+++ crud-mongodb/routes/index.js
@@ -2,17 +2,71 @@ var express = require('express');
 var router = express.Router();
 const db = require("../db");
 
-/* GET home page. */
-router.get('/', async (req, res, next) => {
-  const result = await db.find();
-  res.render('index', { title: 'Express', result });
+router.get('/new', (req, res, next) => {
+  res.render('new', { title: 'Novo Cadastro', doc: { "name": "", "age": "" }, action: '/new' });
 });
 
-router.post("/save", async (req, res) => {
-  const customer = req.body;
-  const result = await db.insert(customer);
-  console.log(result);
-  res.json(result);
+router.get('/edit/:id', async (req, res, next) => {
+  const id = req.params.id;
+
+  try {
+    const doc = await db.findOne(id);
+    res.render('new', { title: 'Edição de Cliente', doc, action: '/edit/' + doc._id });
+  } catch (err) {
+    next(err);
+  }
+})
+router.get('/delete/:id', async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const result = await db.deleteOne(id);
+    console.log(result);
+    res.redirect('/');
+  } catch (err) {
+    next(err);
+  }
+})
+
+router.get('/:pagina?', async (req, res, next) => {
+  const pagina = parseInt(req.params.pagina || "1");
+
+  try {
+    const docs = await db.findAll(pagina);
+    const count = await db.countAll();
+    const qtdPaginas = Math.ceil(count / db.TAMANHO_PAGINA);
+    res.render('index', { title: 'Lista de Clientes', docs, count, qtdPaginas, pagina });
+  } catch (err) {
+    next(err);
+  }
+})
+
+
+router.post('/new', async (req, res, next) => {
+  const name = req.body.name;
+  const age = parseInt(req.body.age);
+
+  try {
+    const result = await db.insert({ name, age });
+    console.log(result);
+    res.redirect('/');
+  } catch (err) {
+    next(err);
+  }
+})
+
+router.post('/edit/:id', async (req, res) => {
+  const id = req.params.id;
+  const name = req.body.name;
+  const age = parseInt(req.body.age);
+
+  try {
+    const result = await db.update(id, { name, age });
+    console.log(result);
+    res.redirect('/');
+  } catch (err) {
+    next(err);
+  }
 })
 
 module.exports = router;
